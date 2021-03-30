4f4d5c9acc49535c472b8c1a28866fca2009745a
Patryk Adamowicz <patryk.adamowicz@student.uwm.edu.pl>
2021-03-30 15:38:47 +0200

    cw4 zad 5

--- cw4.py
+++ cw4.py
@@ -77,9 +77,40 @@ print(produkt.ile_kosztuje())
 # policz_sume – liczy sume elementow
 # policz_elementy – liczy elementy jeśli pierwsza wartość i różnica jest podana
 # Stwórz instancję klasy i sprawdź działanie wszystkich metod.
-def Ciag():
-	pass
+class Ciag:
+	ciagi = []
+	a1 = None
+	n = None
+	r = None
 
+	def wyswietl_dane(self):
+		for ciag in self.ciagi:
+			print(ciag, end="\t")
+		print('')
+
+	def pobierz_elementy(self):
+		ciag = int(input("Podaj liczbe: "))
+
+		self.ciagi.append(ciag)
+
+	def pobierz_parametry(self):
+		self.a1 = int(input("Podaj a1: "))
+		self.n = int(input("Podaj n: "))
+		self.r = int(input("Podaj r: "))
+
+	def policz_sume(self):
+		return sum(self.ciagi)
+
+	def policz_elementy(self):
+		if self.a1 is not None and self.r is not None:
+			return len(self.ciagi)
+
+ciag = Ciag()
+ciag.pobierz_elementy()
+ciag.pobierz_parametry()
+print(ciag.policz_sume())
+print(ciag.policz_elementy())
+ciag.wyswietl_dane()
 
 # Zad. 6
 #
